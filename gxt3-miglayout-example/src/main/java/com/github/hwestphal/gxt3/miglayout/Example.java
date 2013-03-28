3b0e6e1a05c8396ae4e76e984558a95baf45cee1
Harald Westphal <h.westphal@gmx.de>
2013-03-28 15:36:45 +0100

    Add some example code.

--- gxt3-miglayout-example/src/main/java/com/github/hwestphal/gxt3/miglayout/Example.java
+++ gxt3-miglayout-example/src/main/java/com/github/hwestphal/gxt3/miglayout/Example.java
@@ -1,11 +1,28 @@
 package com.github.hwestphal.gxt3.miglayout;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class Example implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
+		Label label = new Label("Hello GWT !!!");
+		Button button = new Button("Say something");
+		button.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				Window.alert("Hello, again");
+			}
+		});
+
+		RootPanel.get().add(label);
+		RootPanel.get().add(button);
 	}
 
 }
