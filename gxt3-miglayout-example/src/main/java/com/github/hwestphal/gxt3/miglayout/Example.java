3a152cd016b33e59124e89a1a0c887af437ba2eb
Harald Westphal <h.westphal@gmx.de>
2013-03-28 15:28:48 +0100

    Add example project.

---
+++ gxt3-miglayout-example/src/main/java/com/github/hwestphal/gxt3/miglayout/Example.java
@@ -0,0 +1,11 @@
+package com.github.hwestphal.gxt3.miglayout;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Example implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+	}
+
+}
